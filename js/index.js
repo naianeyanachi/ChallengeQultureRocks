ff8a12307ca0d77358c3e31c9d507f28e34c5aea
naiane <naiane.yanachi@gmail.com>
2019-11-03 17:15:41 -0300

    Adding some style to index

--- js/index.js
+++ js/index.js
@@ -31,32 +31,44 @@ var app = {
     },
     populateTable: function (params, data){
         var users = document.getElementById("listUsers");
-        var row = users.insertRow(-1);
+        var header = users.createTHead();
+        var row = header.insertRow(-1);
         row.classList.add("header");
-        var cellName = row.insertCell(0);
+        var cellName = document.createElement('th');
         cellName.innerHTML = "Name"
-        var cellJobTitle = row.insertCell(1);
+        cellName.scope = "col";
+        var cellJobTitle = document.createElement('th');
         cellJobTitle.innerHTML = "Job Title"
-        var cellDate = row.insertCell(2);
+        cellJobTitle.scope = "col";
+        var cellDate = document.createElement('th');
         cellDate.innerHTML = "Admission date"
-        var cellEmail = row.insertCell(3);
+        cellDate.scope = "col";
+        var cellEmail = document.createElement('th');
         cellEmail.innerHTML = "Email"
-    
+        cellEmail.scope = "col";
+        
+        row.appendChild(cellName);
+        row.appendChild(cellJobTitle);
+        row.appendChild(cellDate);
+        row.appendChild(cellEmail);
+        
+        
+        var body = users.createTBody();
         data.users.forEach(function(user, index){
-            row = users.insertRow(-1);
+            var newRow = body.insertRow();
             if(index % 2 == 0){
-                row.classList.add("par");
+                newRow.classList.add("par");
             } else {
-                row.classList.add("impar");
+                newRow.classList.add("impar");
             }
 
-            cellName = row.insertCell(0);
+            cellName = newRow.insertCell(0);
             cellName.innerHTML = user.name;
-            cellJobTitle = row.insertCell(1);
+            cellJobTitle = newRow.insertCell(1);
             cellJobTitle.innerHTML = user.job_title;
-            cellDate = row.insertCell(2);
+            cellDate = newRow.insertCell(2);
             cellDate.innerHTML = user.admission_date;
-            cellEmail = row.insertCell(3);
+            cellEmail = newRow.insertCell(3);
             cellEmail.innerHTML = user.email;
 
         });
@@ -74,24 +86,31 @@ var app = {
         //create dropdown
         var pageSelector = document.createElement("select");
         pageSelector.id = "pageSelector"
+        pageSelector.classList.add("btn");
+        pageSelector.classList.add("btn-secondary");
         
         pages = Math.ceil(data.users.length / 10)
         
         for(var i = 1; i <= pages; i++){
-            pageSelector.appendChild(new Option(i, i));
+            option = new Option(i, i);
+            pageSelector.appendChild(option);
         }
         pageSelector.value = params["page"];
         
         
-        //create buttons, if necessary
+        //create nav buttons, if necessary
         if (params["page"] > 1){
             var buttonFirst = document.createElement("BUTTON");
             buttonFirst.id = "btnFirst";
             buttonFirst.innerHTML = "<<";
+            buttonFirst.classList.add("btn");
+            buttonFirst.classList.add("btn-secondary");
             
             var buttonBack = document.createElement("button");
             buttonBack.id = "btnBack";
             buttonBack.innerHTML = "<";
+            buttonBack.classList.add("btn");
+            buttonBack.classList.add("btn-secondary");
             
             document.getElementById("navigation").appendChild(buttonFirst);
             document.getElementById("navigation").appendChild(buttonBack);
@@ -99,16 +118,22 @@ var app = {
             document.getElementById('btnBack').addEventListener('click',function() {app.changePage(parseInt(params["page"]) - 1)});
         }
         
+        //add dropdown in the middle of buttons
         document.getElementById("navigation").appendChild(pageSelector);
+        document.getElementById('pageSelector').addEventListener('change',this.onChange);   
         
         if (params["page"] < pages){
             var buttonLast = document.createElement("BUTTON");
             buttonLast.id = "btnLast";
             buttonLast.innerHTML = ">>";
+            buttonLast.classList.add("btn");
+            buttonLast.classList.add("btn-secondary");
             
             var buttonForward = document.createElement("BUTTON");
             buttonForward.id = "btnForward";
             buttonForward.innerHTML = ">";
+            buttonForward.classList.add("btn");
+            buttonForward.classList.add("btn-secondary");
             
             document.getElementById("navigation").appendChild(buttonForward);
             document.getElementById("navigation").appendChild(buttonLast);
@@ -116,8 +141,7 @@ var app = {
             document.getElementById('btnForward').addEventListener('click', function() {app.changePage(parseInt(params["page"]) + 1)}, false);
         }
         
-        //create event listeners
-        document.getElementById('pageSelector').addEventListener('change',this.onChange);
+        
     },
     listUsers: function (){
         var params = this.getUrlParams();
